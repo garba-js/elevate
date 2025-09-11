7e419c1601035b4b76a1800f79515bb68f4075f5
garba-js <joegarba98@gmail.com>
2025-09-11 12:06:48 +0100

    Portfolio

---
+++ Portfolio/scripts/header.js
@@ -0,0 +1,13 @@
+const navButton = document.querySelector('#nav-button');
+const navBar = document.querySelector('#nav-bar');
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('show');
+  navBar.classList.toggle('show');
+ 
+});
+
+// ✅ Display Current Year and Last Modified Date
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
